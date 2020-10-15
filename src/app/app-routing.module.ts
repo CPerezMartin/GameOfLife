a049c0be48ff27d857b0e3d16d2a214d9b644586
Carlos Pérez Martín <cperez@makenai.es>
2020-10-15 17:45:15 +0200

    routes created

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GolComponent } from './gol/gol.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AngularDocComponent } from './angular-doc/angular-doc.component';
+
+const routes: Routes = [
+   { path: '', component: GolComponent },
+   { path: 'gol', component: GolComponent },
+   { path: 'angular-documentation', component: AngularDocComponent }
+];
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GolComponent } from './gol/gol.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AngularDocComponent } from './angular-doc/angular-doc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GolComponent,
+    AngularDocComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
