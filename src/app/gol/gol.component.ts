0781ae83c4012562b6651cc733ce7abe5329fa04
Carlos Pérez Martín <cperez@makenai.es>
2020-10-16 14:03:36 +0200

    grid created

--- src/app/gol/gol.component.ts
+++ src/app/gol/gol.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵCompiler_compileModuleSync__POST_R3__ } from '@angular/core';
 
 @Component({
   selector: 'app-gol',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GolComponent implements OnInit {
 
-  constructor() { }
+  rowMax = 25;
+  colMax = 50;
+  rows = [];
+
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+   this.rows = this.createGrid(this.rowMax, this.colMax);
+   console.log('CONSOLE:: GolComponent -> ngOnInit -> this.rows', this.rows);
+   console.log('CONSOLE:: GolComponent -> ngOnInit -> this.rows.cols', this.rows[5].cols);
+  }
+
+  createGrid = (rows: number, cols: number) => {
+    const output = [];
+    for (let i = 0; i < rows; i++){
+      const col = [];
+      for ( let j = 0; j < cols; j++){
+        const cell = {
+          active: false,
+          neighbours: 0
+        };
+        col.push(cell);
+      }
+      output.push({cols: col});
+    }
+    return output;
   }
 
 }
