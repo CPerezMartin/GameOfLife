b06912659d7cf05ec9721afb1604e3ea950defb3
Carlos Pérez Martín <cperez@makenai.es>
2020-10-19 16:12:09 +0200

    adjusting default values

--- src/app/gol/gol.component.ts
+++ src/app/gol/gol.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit} from '@angular/core';
 })
 export class GolComponent implements OnInit {
 
-  rowMax = 25;
+  rowMax = 50;
   colMax = 50;
-  randomValue = 100;
+  randomValue = 200;
   rows = [];
   generation = 0;
   timer;
